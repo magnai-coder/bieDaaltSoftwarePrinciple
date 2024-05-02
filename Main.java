a3a8d615a13eb2b45a6e2a52b7a03141d4f27083
Bayaraa <bayarlvl@gmail.com>
2024-05-03 00:00:42 +0800

    tailbar oruulsan

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
+        //Гаднаас for(x=3;x<5;x++) өгөгдөл орно
             Scanner intLine = new Scanner(System.in);
             String input = intLine.nextLine();
             Lexer checkIt = new Lexer(input);
+        //Орж ирсэн оролтын Lexer класс ашиглан ялгаж алдааг илрүүлэх боломжтой болгон.
             For print = checkIt.divideCheckCode();
+        //Оролтын өгүүлбэр зүй утга зүйг шалгаж бүрэн болсон үр дүнг toString-ээр хэвлэн.
             System.out.println(print.toString());  
         } 
     }
